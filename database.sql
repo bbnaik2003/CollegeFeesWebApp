d542633042b4102fe3534a29257f3bf1b21cadf7
B B NAIK <bbnaik589@gmail.com>
2025-05-30 02:03:20 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,11 @@
+Database Structure:
 
+  
+CREATE TABLE FeePayments (
+PaymentID INT PRIMARY KEY AUTO_INCREMENT,
+StudentID INT,
+StudentName VARCHAR(100),
+PaymentDate DATE,
+Amount DECIMAL(10,2),
+Status VARCHAR(20) -- e.g., Paid, Overdue
+);
